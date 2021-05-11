77dbdc23dd4368483e5447af4f1686ad0bbe9ba1
alex-beh <chunjye088@gmail.com>
2021-05-11 16:39:36 +0800

    add paired into Leg.msg and fix the python and launch file issue

--- scripts/joint_leg_tracker.py
+++ scripts/joint_leg_tracker.py
@@ -344,7 +344,7 @@ class KalmanMultiTracker:
                 leg_clusters.add(detect)
         non_leg_clusters = detected_clusters_set.difference(leg_clusters)
         for detect in non_leg_clusters:
-            non_leg = Leg(Point(detect.pos_x, detect.pos_y, 0), 1)
+            non_leg = Leg(Point(detect.pos_x, detect.pos_y,0), 0, False,[],0,0)
             non_legs_msg.legs.append(non_leg)              
         self.non_leg_clusters_pub.publish(non_legs_msg)  
 
